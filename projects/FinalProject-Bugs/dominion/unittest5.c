915001fd2fe589ae55af0fd685c0eff3b159adda
Maryum Shabazz <49971083+Rebelworld@users.noreply.github.com>
2019-12-02 19:57:30 -0600

    Add files via upload

--- projects/FinalProject-Bugs/dominion/unittest5.c
+++ projects/FinalProject-Bugs/dominion/unittest5.c
@@ -3,7 +3,7 @@ Name: Maryum Shabazz
 Date: 11/30/19
 Description: Test for Bug 5
 */
-​
+
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
@@ -12,8 +12,8 @@ Description: Test for Bug 5
 #include "rngs.h"
 #include <time.h>
 #include <stdlib.h>
-​
-​
+
+
 //Values to compare functions
 int assertValues(int paramOne, int paramTwo)
 {
@@ -26,7 +26,7 @@ int assertValues(int paramOne, int paramTwo)
         return 0;
     }
 }
-​
+
 int main() {
     int seed = 100;
     int numPlayer=2;
@@ -36,10 +36,10 @@ int main() {
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, minion};
     struct gameState G, testG;
-    printf("Testing bug 5\n");
-​
+    printf("Testing bug 5\n");  
 
-    //initialize game
+    
+    //initialize game 
    	initializeGame(numPlayer,k,seed,&G);
     G.handCount[0] = 2;
   //Setup player hand with 1 mine and 1 copper
@@ -47,19 +47,19 @@ int main() {
   G.supplyCount[mine]--;
   G.hand[0][1] = copper;
   G.supplyCount[copper]--;
-​
+
   G.discardCount[0]=3;
-  //setup player discard pile with 1 mine and 2 coppers
+  //setup player discard pile with 1 mine and 2 coppers 
   G.discard[0][0]=copper;
   G.supplyCount[copper]--;
   G.discard[0][1]=copper;
   G.supplyCount[copper]--;
   G.discard[0][2] = mine;
   G.supplyCount[mine]--;
-​
 
+  
   G.deckCount[0]=5;
-​
+
   //set up player deck pile with 3 curses and 2 estates
   G.deck[0][0]=curse;
   G.supplyCount[curse]--;
@@ -71,12 +71,12 @@ int main() {
   G.supplyCount[estate]--;
   G.deck[0][4]=estate;
   G.supplyCount[estate]--;
-​
+
   //set player to first player
   int player=0;
   int score=scoreFor(player,&G);
-​
-​
+
+
   if( assertValues(score,-1))
   {
   	printf("Pass: Score for player one  is correct\n");
@@ -85,10 +85,10 @@ int main() {
   {
   	printf("Fail: Score for player one  is not correct\n");
   }
-​
 
-  printf("END TEST bug 5\n");
-  printf("******************************************************************************************************\n");
-  return 0;   
-​
+    	
+printf("END TEST bug 5\n");
+printf("******************************************************************************************************\n");
+return 0;   
+
 }
